dfaadb90014dc9e484501dfca9591db3807cb58d
Neeraj Kumar <140229390+Neeraj7911@users.noreply.github.com>
2024-08-31 21:14:59 +0530

    Add files via upload
    frequency code

---
+++ fre.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class fre{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= sc.nextInt();
+        }
+        countfre(arr);
+        sc.close();
+    }
+    public static void countfre(int arr[]){
+        Arrays.sort(arr);
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            int count=1;
+            while(i<n-1 && arr[i] == arr[i+1]){
+                count++;
+                i++;
+            }
+            System.out.println(arr[i]+" "+count);
+        }
+    }
+}
