86e14fd1eda01823ca251dc349b320ab3a9315ae
neeraj7911 <ns1399@srmist.edu.in>
2025-01-07 18:42:10 +0530

    reversestring

---
+++ revstr.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class revstr {
+    public static String revstr(String s){
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int start = 0;
+        int end = n-1;
+        while(start<end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return new String(arr);
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s = sc.nextLine();
+        String result = revstr(s);
+        
+
+
+    }
+}
