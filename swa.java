eed9a182577d0814bcb01c7d258653c6f182671e
neeraj7911 <ns1399@srmist.edu.in>
2025-01-08 17:08:58 +0530

    swap

---
+++ swa.java
@@ -0,0 +1,13 @@
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+        //int temp = a;
+        //a=b;
+        //b=temp;
+        List<Integer> result = new ArrayList<>();
+        result.add(b);
+        result.add(a);
+        return result;
+    }
+}
