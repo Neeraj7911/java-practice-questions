de15dd8460c56a45a5c6f4041841f3f4903eccc2
neeraj7911 <ns1399@srmist.edu.in>
2025-01-08 17:07:51 +0530

    factorial

---
+++ Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    static int factorial(int n) {
+        // code here
+        int fact=1;
+        for(int i=1; i<=n; i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+} {
+    
+}
