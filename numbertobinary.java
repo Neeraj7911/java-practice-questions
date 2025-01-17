7187e3d875529134ab0ecf260386279563ba0899
neeraj7911 <ns1399@srmist.edu.in>
2025-01-17 17:47:33 +0530

    number to binary

---
+++ numbertobinary.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class numbertobinary{
+    public static String getBinaryNo(int N){
+        StringBuilder S = new StringBuilder();
+        String number = Integer.toBinaryString(N);
+        int noOfZeroes = 30-number.length();
+        for(int i=0; i<noOfZeroes; i++){
+            S.append('0');
+        }
+        S.append(number);
+        return S.toString();
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(getBinaryNo(N));
+    }
+}
