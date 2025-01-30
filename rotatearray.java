40d095541d70f39388b427189c0bcb5669683537
neeraj7911 <ns1399@srmist.edu.in>
2025-01-30 20:11:05 +0530

    rotate matrix

---
+++ rotatearray.java
@@ -0,0 +1,42 @@
+/*Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
+
+Examples:
+
+Input: mat[][] = [[1, 2, 3],
+                [4, 5, 6]
+                [7, 8, 9]]
+Output: Rotated Matrix:
+[3, 6, 9]
+[2, 5, 8]
+[1, 4, 7]
+Input: mat[][] = [[1, 2],
+                [3, 4]]
+Output: Rotated Matrix:
+[2, 4]
+[1, 3] */
+
+import java.util.*;
+public class rotatearray {
+    public static void rotate(int[][] matrix){
+        int n = matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0; j<i; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+            }
+        }
+        for(int i=0; i<n; i++){
+            int a=0, b=n-1;
+            while(a<b){
+                int temp = matrix[i][a];
+                matrix[i][a] = matrix[i][b];
+                matrix[i][b] = temp;
+                a++;
+                b--;
+            }
+        }
+    }
+    
+}
